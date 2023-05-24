7d6d42b7e3b04dc054d88b7e1bf631e294af2fa0
umar-abdullah22 <umar.coderzhunt@gmail.com>
2023-05-24 18:22:15 +0500

    updated fro mysql

--- backend/src/config/orm.config.ts
+++ backend/src/config/orm.config.ts
@@ -7,7 +7,7 @@ const ENTITIES_DIR = resolve(__dirname, '../', '**', 'entities', '*.entity.{ts,j
 const MIGRATIONS_DIR = resolve(__dirname, '../', '**', 'migrations', '*.{ts,js}');
 const ORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'zizle.cmohmyt3pz7h.eu-north-1.rds.amazonaws.com',
   port: +process.env.DB_PORT || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'password',
